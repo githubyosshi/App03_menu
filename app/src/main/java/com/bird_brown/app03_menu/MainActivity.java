41bee4a5edbb9bc07b196075e0250251b9a9cc5e
githubyosshi <kanrisya.shisutemu@gmail.com>
2020-01-22 09:49:41 +0900

    first commit

---
+++ app/src/main/java/com/bird_brown/app03_menu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bird_brown.app03_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
